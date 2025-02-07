db0657bc1c6fd8e4cc742b434f3582504cc9de9a
mehmet ali <mgul21969@gmail.com>
2025-02-07 18:29:37 +0300

    Anasayfa oluşturuldu

---
+++ untitled/src/Anasayfa.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Anasayfa {
+    JFrame frame = new JFrame();
+
+    public Anasayfa(){
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Anasayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
