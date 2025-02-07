264e4e9f2c63b3696dbf04320483e2aac755520f
mehmet ali <mgul21969@gmail.com>
2025-02-07 18:35:34 +0300

    Button eklendi

--- untitled/src/Anasayfa.java
+++ untitled/src/Anasayfa.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Anasayfa {
+public class Anasayfa implements ActionListener {
     JFrame frame = new JFrame();
+    JButton button = new JButton("Profile Göster");
 
     public Anasayfa(){
+        button.addActionListener(this);
+        button.setBounds(200,200,100,50);
+        frame.add(button);
+
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Anasayfa");
         frame.setLayout(null);
         frame.setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
